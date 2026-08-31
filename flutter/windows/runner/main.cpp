b0784b7894dc5ebc2adc7c99a1b65a8f7fe72c98
Bruno Willian Custodio <brunowillianc@gmail.com>
2026-08-31 15:23:28 -0300

    Change default window size to 320x440

--- flutter/windows/runner/main.cpp
+++ flutter/windows/runner/main.cpp
@@ -138,7 +138,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   Win32Window::Point relative_origin(10, 10);
 
   Win32Window::Point origin(workarea_origin.x + relative_origin.x, workarea_origin.y + relative_origin.y);
-  Win32Window::Size size(360u, 620u);
+  Win32Window::Size size(320u, 440u);
 
   // Fit the window to the monitor's work area.
   Win32Desktop::FitToWorkArea(origin, size);
